b4a045bc947960a82b060c56e7e72de665ff3d19
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-07-24 21:19:43 +0300

    styling the buttons for fetch and reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <div className='container'>
-        <div className='data-window'>
-          <span className='title'>JSONPlaceholder API</span>
+      <div className="container">
+        <div className="data-window">
+          <span className="title">JSONPlaceholder API</span>
+          <div className="sidebar">
+            <button className="fetch-button">Fetch Users</button>
+            <button className="reset-button">Reset</button>
+          </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
