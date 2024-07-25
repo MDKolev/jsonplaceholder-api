1fd0558e32f55e5fddf6f1db5cba07666e886316
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-07-25 21:30:23 +0300

    creation and styling of buttons for page number

--- src/App.jsx
+++ src/App.jsx
@@ -46,9 +46,7 @@ function App() {
             <div className="button-container">
               <div className="pagination-settings">
                 <label htmlFor="itemsPerPage">Items per page:</label>
-                <select
-                  id="itemsPerPage"
-                >
+                <select id="itemsPerPage">
                   <option value={1}>1</option>
                   <option value={3}>3</option>
                   <option value={5}>5</option>
@@ -96,6 +94,12 @@ function App() {
                   ))}
                 </tbody>
               </table>
+              <div className="pagination">
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>4</button>
+              </div>
             </div>
           </div>
         </div>
