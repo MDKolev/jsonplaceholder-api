83fcbcd360c6e30c4cd83549383e38938c7a7c78
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-07-27 21:26:06 +0300

    set an error prop for the ErrorPage component

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,21 @@ import { Toaster } from "sonner";
 import "./App.css";
 import FetchData from "./components/FetchData";
 import ErrorPage from "./components/ErrorPage";
+import { useState } from "react";
 
 function App() {
-  
+  const [error, setError] = useState(null);
 
   return (
     <>
       <div className="main-container">
         <div className="data-window">
-          {<ErrorPage/>}
-          {/* <FetchData/> */}
-          <Toaster richColors duration={2000}/>
+          {error !== null ? (
+            <FetchData setError={setError} />
+          ) : (
+            <ErrorPage error={error} />
+          )}
+          <Toaster richColors duration={2000} />
         </div>
       </div>
     </>
--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
-import './errorPage.css'
+import React from "react";
+import "./errorPage.css";
 
-const ErrorPage = () => {
+const ErrorPage = ({ error }) => {
   return (
     <>
-    <div className='header'>
-      <div>
-        <h1 className='title'>JSONPlaceholder</h1>
-        <div className='error-container'>
-          <span className='error-occured'>An Error has occured!</span>
-          <span className='error-message'>error message</span>
+      <div className="header">
+        <div>
+          <h1 className="title">JSONPlaceholder API</h1>
+          <div className="error-container">
+            {error === null ? (
+              <div className="no-errors">No present errors.</div>
+            ) : (
+              <>
+                <span className="error-occured">An Error has occured!</span>
+                {/* <span className="error-message">{error.message}</span> */}
+              </>
+            )}
+          </div>
         </div>
       </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default ErrorPage
+export default ErrorPage;
